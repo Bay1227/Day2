b5e77c279e53dfe8d24f1292e55ea48f6ca346a9
Bayartsengel Battulga <tsengelbattulga1227@gmail.com>
2022-06-17 20:49:27 -0700

    Initial commit

---
+++ src/Sum.java
@@ -0,0 +1,12 @@
+public class Sum {
+    public static void main(String[] args){
+        byte byteNum = (byte) 3349;
+        short shortNum = 90;
+        int intNum = 1;
+        long longNum = 50000 + (10*byteNum) + intNum + shortNum;
+        System.out.println(longNum);
+
+
+
+    }
+}
